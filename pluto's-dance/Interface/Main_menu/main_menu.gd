3e52a28daf085bf4085600ecaf1e8468ad956f42
justapoint <arseniikulaev2k4@gmail.com>
2025-02-25 13:11:35 +0100

    Main_menu fix 0.2

--- pluto's-dance/Interface/Main_menu/main_menu.gd
+++ pluto's-dance/Interface/Main_menu/main_menu.gd
@@ -1,11 +1,30 @@
 extends Control
 
+@onready var Settings = $B_Settings
+@onready var Play = $B_Settings
+@onready var Exit = $B_Settings
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
+	Settings.visible = false
+
+
+func _on_play_pressed() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_settings_pressed() -> void:
+	Settings.visible = true
+	Play.visible = false
+	$MarginContainer/VBoxContainer/Settings.visible = false
+	Exit.visible = false
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_back_pressed() -> void:
+	Settings.visible = false
+	Play.visible = true
+	$MarginContainer/VBoxContainer/Settings.visible = true
+	Exit.visible = true
