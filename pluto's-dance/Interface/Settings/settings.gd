9ab2229c996f3b62fdcfe57faf94af1f1c0cf33b
justapoint <arseniikulaev2k4@gmail.com>
2025-02-25 12:26:57 +0100

    AudioServer fix and Settings re

--- pluto's-dance/Interface/Settings/settings.gd
+++ pluto's-dance/Interface/Settings/settings.gd
@@ -6,9 +6,9 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-func _on_volume_value_changed(value: float) -> void:
-	pass # Replace with function body.
+func _on_volume_value_changed(value):
+	print("volume - " + str(value))
 
 
-func _on_music_value_changed(value: float) -> void:
-	pass # Replace with function body.
+func _on_music_value_changed(value):
+	print("music - " + str(value))
