c5dafff80504908e2c7397d841d1e7abae89cfae
justapoint <arseniikulaev2k4@gmail.com>
2025-02-25 12:11:43 +0100

    AudioServer and Setting set

---
+++ pluto's-dance/Interface/Settings/settings.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _on_volume_value_changed(value: float) -> void:
+	pass # Replace with function body.
+
+
+func _on_music_value_changed(value: float) -> void:
+	pass # Replace with function body.
