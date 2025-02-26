31f86e9203f823bfd1bc1314f253624ca4ab815c
justapoint <arseniikulaev2k4@gmail.com>
2025-02-26 11:57:42 +0100

    Audio Settings

--- pluto's-dance/Interface/Settings/settings.gd
+++ pluto's-dance/Interface/Settings/settings.gd
@@ -1,22 +1,27 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		$MarginContainer/VBoxContainer/Fullscreen.button_pressed = true
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		
+	GlobalAudioServer.emit_signal("change_music", lerp(-40.0, 0.0, $MarginContainer/VBoxContainer/Music.value / 100.0))
+	GlobalAudioServer.emit_signal("change_volume", lerp(-40.0, 0.0, $MarginContainer/VBoxContainer/Volume.value / 100.0))
 
 
 func _on_volume_value_changed(value):
-	print("volume - " + str(value))
+	var volume_db = lerp(-40.0, 0.0, value / 100.0)  # Преобразуем значение ползунка в дБ
+	GlobalAudioServer.emit_signal("change_volume", value)
 
 
-func _on_music_value_changed(value):
-	print("music - " + str(value))
+func _on_music_value_changed(value: float):
+	var volume_db = lerp(-40.0, 0.0, value / 100.0)  # Преобразуем значение ползунка в дБ
+	GlobalAudioServer.emit_signal("change_music", volume_db)  # Передаем громкость в дБ
 
 
 func _on_fullscreen_toggled(toggled_on):
-	print("Fullscreen - " + str(toggled_on))
-	
 	if toggled_on:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
--- pluto's-dance/Scripts/AudioServer.gd
+++ pluto's-dance/Scripts/AudioServer.gd
@@ -1,6 +1,22 @@
 extends Node
 
+signal change_volume(value)
+signal change_music(value)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var test_settings = load("res://Assets/Sounds/StreetAmbient_normal.ogg") #переменная для теста настроек звука
+
+
+func _ready():
+	change_volume.connect(change_volume_f)
+	change_music.connect(change_music_f)
+	
+	$Music.stream = test_settings
+	$Music.play()
+	
+	
+func change_volume_f(value):
+	$Volume.volume_db = value + 15
+	
+	
+func change_music_f(value):
+	$Music.volume_db = value + 15
