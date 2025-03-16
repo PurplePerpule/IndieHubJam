63181670050b720ee8cbf6f8b7ad927cc5a7d4e1
Purple <dhakut@yandex.ru>
2025-03-16 23:49:17 +0300

    little fix

--- pluto's-dance/Scripts/unvitain.gd
+++ pluto's-dance/Scripts/unvitain.gd
@@ -1,10 +1,12 @@
 extends RigidBody3D
 
 
-
+var flag = false
 func interact():
-	$"..".state2 = false
-	$"../Narkoman/CloseEyes/Eyes".play("close")
-	$AudioStreamPlayer3D.playing = true
-	await get_tree().create_timer(2).timeout
-	LevelManager.load_level("res://Scenes/dripscene/dream_lvl_1.tscn")
+	if flag == false:
+		flag = true
+		$"..".state2 = false
+		$"../Narkoman/CloseEyes/Eyes".play("close")
+		$AudioStreamPlayer3D.playing = true
+		await get_tree().create_timer(2).timeout
+		LevelManager.load_level("res://Scenes/dripscene/dream_lvl_1.tscn")
